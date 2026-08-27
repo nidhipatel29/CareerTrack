d283ed7ac8592835a9c8ee5092aeefb953242cd2
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-27 12:23:46 -0400

    add jobJpaRepo

---
+++ src/main/java/com/CareerTrack/repository/JobRepository.java
@@ -0,0 +1,7 @@
+package com.CareerTrack.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.CareerTrack.entity.Job;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+    
+}
