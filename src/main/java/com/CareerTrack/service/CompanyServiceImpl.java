90187e0eb7b2232eb7a937d9c5e62b9fa7d1a44e
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 12:50:52 -0400

    Add company service and create company logic

--- src/main/java/com/CareerTrack/service/CompanyServiceImpl.java
+++ src/main/java/com/CareerTrack/service/CompanyServiceImpl.java
@@ -1,5 +1,38 @@
 package com.CareerTrack.service;
 
-public class CompanyServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.CareerTrack.dto.CompanyRequest;
+import com.CareerTrack.dto.CompanyResponse;
+import com.CareerTrack.entity.Company;
+import com.CareerTrack.repository.CompanyRepository;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository theCompanyRepository){
+      this.companyRepository=theCompanyRepository;
+    }
+    @Override
+    public CompanyResponse createCompany(CompanyRequest request) {
+
+        //now we need to save the entity not a request object
+        //convert companyRequest to company entity 
+        Company company=new Company(
+                                    request.getName(),request.getDescription(),request.getWebsite(),request.getLocation());
+    
+        
+        Company tempCompany=companyRepository.save(company);
+
+        //return response
+        //convert company entity to company response
+
+        CompanyResponse companyResponse=new CompanyResponse(tempCompany.getId(),tempCompany.getName(),tempCompany.getDescription()
+                                                            ,tempCompany.getWebsite(),tempCompany.getLocation(),tempCompany.getCreatedAt());
+        return companyResponse;
+    }
     
 }
