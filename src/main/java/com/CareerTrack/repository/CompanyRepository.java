3499fd70dce63300e73b6e860585ce7e59e83c48
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 18:42:02 -0400

    made no changes

--- src/main/java/com/CareerTrack/repository/CompanyRepository.java
+++ src/main/java/com/CareerTrack/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package com.CareerTrack.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.CareerTrack.dto.CompanyResponse;
 import com.CareerTrack.entity.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long>{
