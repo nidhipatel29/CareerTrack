586e5b6bb1956c4ebfeabfee39c53f5e9f57e43f
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 18:40:02 -0400

    Add CompanyNotFoundException for missing companies

---
+++ src/main/java/com/CareerTrack/exception/CompanyNotFoundException.java
@@ -0,0 +1,9 @@
+package com.CareerTrack.exception;
+
+public class CompanyNotFoundException extends RuntimeException{
+
+    public CompanyNotFoundException(String message){
+         super(message);
+    }
+    
+}
