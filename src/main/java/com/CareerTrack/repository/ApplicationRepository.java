d0653c7799c89880d0b5a440e407c727cbe3a983
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 10:49:55 -0400

    change the package name

--- src/main/java/com/CareerTrack/repository/ApplicationRepository.java
+++ src/main/java/com/CareerTrack/repository/ApplicationRepository.java
@@ -1,7 +1,6 @@
 package com.CareerTrack.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.CareerTrack.Application;
-
+import com.CareerTrack.entity.Application;
 public interface ApplicationRepository extends JpaRepository<Application,Long>{
     
 }
