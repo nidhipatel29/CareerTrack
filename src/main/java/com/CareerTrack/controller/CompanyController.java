c3cc1c674b76cba77acf935e54ace051b52336e2
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 19:31:46 -0400

    Add company update endpoint

--- src/main/java/com/CareerTrack/controller/CompanyController.java
+++ src/main/java/com/CareerTrack/controller/CompanyController.java
@@ -13,6 +13,8 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -47,6 +49,11 @@ public class CompanyController {
       return companyService.getCompanyById(id);
     }
     
+    @PutMapping("/{id}")
+    public ResponseEntity<CompanyResponse> updateCompany(@PathVariable Long id, @Valid @RequestBody CompanyRequest request){
+      CompanyResponse updatedCompany = companyService.updateCompany(id, request);
+      return ResponseEntity.ok(updatedCompany);
+    }
     
     
 }
