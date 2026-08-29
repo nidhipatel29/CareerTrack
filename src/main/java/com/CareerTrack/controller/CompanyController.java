1b88005b26ae97c341088a29f3bfc3d0cd8d7646
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 15:10:50 -0400

    remove line spacing

--- src/main/java/com/CareerTrack/controller/CompanyController.java
+++ src/main/java/com/CareerTrack/controller/CompanyController.java
@@ -1,21 +1,16 @@
 package com.CareerTrack.controller;
-
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.CareerTrack.dto.CompanyRequest;
 import com.CareerTrack.dto.CompanyResponse;
 import com.CareerTrack.service.CompanyService;
-
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
