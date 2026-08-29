520cfe143606a7075bcb331c502e0546bb8c4e41
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-08-29 14:35:34 -0400

    add user friendly exceptions

---
+++ src/main/java/com/CareerTrack/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.CareerTrack.exception;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+   
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleException(MethodArgumentNotValidException exception){
+
+        Map<String,String> errors=new HashMap<>();
+
+        //store the field errors
+        List<FieldError> feildErrors = exception.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : feildErrors) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+
+    }
+}
