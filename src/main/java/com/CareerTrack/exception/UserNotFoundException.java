5ecbc1eb7a2a54cf48b985189444707149a3c441
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-04 15:00:39 -0400

    hadle user not found exception in this class

---
+++ src/main/java/com/CareerTrack/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.CareerTrack.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    
+    public UserNotFoundException(String message){
+        super(message);
+
+    }
+}
