a512e0e760ba3f2882d0e6a7c8a337de7e2e675d
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-04 14:56:10 -0400

    no major changes

--- src/main/java/com/CareerTrack/Application.java
+++ src/main/java/com/CareerTrack/Application.java
@@ -3,11 +3,18 @@ package com.CareerTrack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.CareerTrack.entity.User;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	public void setUser(User user) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'setUser'");
 	}
 
 }
