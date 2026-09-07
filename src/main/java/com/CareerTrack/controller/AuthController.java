554fb4b962883341eac0b9ecf474ab22e3b23f28
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-07 16:10:34 -0400

    Add user registration endpoint

---
+++ src/main/java/com/CareerTrack/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.CareerTrack.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.CareerTrack.dto.RegisterRequest;
+import com.CareerTrack.dto.RegisterResponse;
+import com.CareerTrack.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController 
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+@PostMapping("/register")
+public RegisterResponse register(@Valid @RequestBody RegisterRequest theRegisterRequest){
+    return  authService.register(theRegisterRequest);
+}
+
+    
+    
+}
