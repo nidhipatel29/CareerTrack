44c44e3a3ec334ccc7ceef9848133ae8e3351016
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-07 15:22:08 -0400

    remove unneccessary imports

--- src/main/java/com/CareerTrack/repository/JobRepository.java
+++ src/main/java/com/CareerTrack/repository/JobRepository.java
@@ -1,10 +1,7 @@
 package com.CareerTrack.repository;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import com.CareerTrack.entity.Company;
 import com.CareerTrack.entity.EmploymentType;
 import com.CareerTrack.entity.Job;
 
