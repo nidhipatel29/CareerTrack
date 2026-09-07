234e7bce4d50c5b8588055dd661ba0351d4df315
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-07 11:21:41 -0400

    Add default values for job pagination and sorting

--- src/main/java/com/CareerTrack/controller/JobController.java
+++ src/main/java/com/CareerTrack/controller/JobController.java
@@ -85,9 +85,10 @@ public class JobController {
 
 @GetMapping("/page")
 public Page<JobResponse> getSelectedJobs(
-        @RequestParam int page,
-        @RequestParam int size,
-      @RequestParam  String sortBy,@RequestParam  String direction) {
+       @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size,
+        @RequestParam(defaultValue = "createdAt") String sortBy,
+        @RequestParam(defaultValue = "desc") String direction) {
 
     return jobService.getJobsWithPagination(page, size,sortBy,direction);
 }
