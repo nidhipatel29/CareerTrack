6eb6369fbda4a170fb959d0b18b8cf07861c6d82
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-07 16:09:53 -0400

    Add email existence check to user repository

--- src/main/java/com/CareerTrack/repository/UserRepository.java
+++ src/main/java/com/CareerTrack/repository/UserRepository.java
@@ -3,4 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.CareerTrack.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+     boolean existsByEmail(String email);
 }
