ea9fae0ddc885a1baad76cd21b34aac225888a8f
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-11 11:47:54 -0400

    add JWT service for token generation and validation

---
+++ src/main/java/com/CareerTrack/service/JwtService.java
@@ -0,0 +1,55 @@
+package com.CareerTrack.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "CareerTrackSuperSecretKeyForJwtToken123456";
+    private final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+
+    // read information back from the token.
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    // token expiration checking
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+    private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+}
+
+}
