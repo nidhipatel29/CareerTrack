52919ba8b8daddb3b261af9dac5b2c124bf95abe
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-09-11 15:34:54 -0400

    Handle JWT token validation exceptions

--- src/main/java/com/CareerTrack/service/JwtService.java
+++ src/main/java/com/CareerTrack/service/JwtService.java
@@ -48,18 +48,15 @@ public class JwtService {
                 .getExpiration();
     }
 
-
     private boolean isTokenExpired(String token) {
-    return extractExpiration(token).before(new Date());
-}
+        return extractExpiration(token).before(new Date());
+    }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String userName = extractUsername(token);
 
-public  boolean isTokenValid(String token,UserDetails userDetails){
-    String userName=extractUsername(token);
-
-   return userDetails.getUsername().equals(userName)
-            && !isTokenExpired(token);
-}
-
+        return userDetails.getUsername().equals(userName)
+                && !isTokenExpired(token);
+    }
 
 }
